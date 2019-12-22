e96f75dd5975124b2e92f9705eda8cbbe67bbb92
Jose Botelho <botelho.j@outlook.com>
2019-12-21 21:46:06 -0500

    First commit, menu and layout components started

---
+++ src/user/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signup = () => (
+  <Layout title="Sign Up" description="Sign up for Node React E-commerce App">
+    ...
+  </Layout>
+);
+
+export default Signup;
